24e6e72615aa409d78c219ad5581cb670a14adf7
SETTER <lphp@mail.ru>
2026-06-14 15:44:30 +0300

    feat: добавить возможность подгрузки конфига из консоли

--- src/log_analyzer/cli.py
+++ src/log_analyzer/cli.py
@@ -1,21 +1,22 @@
-import tomllib  # Python 3.11+, если версия ниже — используй tomli
+import tomllib
 from pathlib import Path
 
 import typer
-
-from log_analyzer.analyzer import DEFAULT_CONFIG, LogAnalyzer
+from log_analyzer.analyzer import LogAnalyzer, DEFAULT_CONFIG
 
 app = typer.Typer()
 
 
 @app.command()
-def main(config_file: Path | None = None):
+def main(
+    config_file: Path | None = typer.Option(None, "--config", help="Путь к файлу конфига")
+):
     config = DEFAULT_CONFIG.copy()
 
     if config_file and config_file.exists():
         with open(config_file, "rb") as f:
             user_config = tomllib.load(f)
-        config.update(user_config)  # обновляем значения из файла
+        config.update(user_config)
 
     log = LogAnalyzer(config=config)
     log.metrics()
