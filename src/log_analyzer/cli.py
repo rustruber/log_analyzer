5aacf235f3ee3d1c54c6d4c141eec5579739dd37
SETTER <lphp@mail.ru>
2026-06-14 15:47:10 +0300

    fix: update cli.py

--- src/log_analyzer/cli.py
+++ src/log_analyzer/cli.py
@@ -2,15 +2,14 @@ import tomllib
 from pathlib import Path
 
 import typer
-from log_analyzer.analyzer import LogAnalyzer, DEFAULT_CONFIG
+
+from log_analyzer.analyzer import DEFAULT_CONFIG, LogAnalyzer
 
 app = typer.Typer()
 
 
 @app.command()
-def main(
-    config_file: Path | None = typer.Option(None, "--config", help="Путь к файлу конфига")
-):
+def main(config_file: Path | None = typer.Option(None, "--config", help="Путь к файлу конфига")):
     config = DEFAULT_CONFIG.copy()
 
     if config_file and config_file.exists():
