5c314d317b734b95c39833e4ce1f9b5e6e7ebb84
setter <lphp@mail.ru>
2026-06-16 15:37:21 +0300

    feat: добавить отображение версии пакета

--- src/log_analyzer/__init__.py
+++ src/log_analyzer/__init__.py
@@ -1,1 +1,4 @@
 """Top-level package for log_analyzer."""
+from importlib.metadata import version as get_version
+
+__version__ = get_version("log-analyzer-cli")
--- src/log_analyzer/cli.py
+++ src/log_analyzer/cli.py
@@ -2,14 +2,20 @@ import tomllib
 from pathlib import Path
 
 import typer
-
+from log_analyzer import __version__
 from log_analyzer.analyzer import DEFAULT_CONFIG, LogAnalyzer
 
 app = typer.Typer()
 
 
 @app.command()
-def main(config_file: Path | None = typer.Option(None, "--config", help="Путь к файлу конфига")):
+def main(config_file: Path | None = typer.Option(None, "--config", help="Путь к файлу конфига"),
+         version: bool = typer.Option(False, "--version", help="Show version and exit")):
+    if version:
+        from log_analyzer import __version__
+        typer.echo(f"log-analyzer-cli {__version__}")
+        raise typer.Exit()
+
     config = DEFAULT_CONFIG.copy()
 
     if config_file and config_file.exists():
